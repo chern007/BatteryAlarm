93bae44e7cff8cd2bf103b9da3151ddcc7bf64db
chern007 <carlos.hernandez_crespo@nokia>
2018-01-24 10:41:25 +0100

    BroadcastReceiver montado y funcionando, simplemente hay que meter los metodos de enviar emails y SMS en el metodo cambiaPorcentaje

--- app/src/main/java/com/example/carlos/batteryalarm/Inicio.java
+++ app/src/main/java/com/example/carlos/batteryalarm/Inicio.java
@@ -12,23 +12,21 @@ import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Inicio extends AppCompatActivity {
 
     private TextView batteryTxt;
     public static SQLiteDatabase db;
-    private int nivelBateriaActual = 100;
     public static int umbral;
     public static String[] listaContactos;
     public static String[] listaEmails;
+
+    //variables relativas al "BroadcastReceiver"
+    private IntentFilter bateryIntentFilter;
+    private BateryLevelBroadcastReceiver bateryLevelBroadcastReceiver;
 
 
     //si vuelve a saltar la actividad cargamos los datos de nuevo
@@ -36,21 +34,11 @@ public class Inicio extends AppCompatActivity {
     public void onResume() {
         super.onResume();
 
-        recuperaDatos();
+        recuperaDatos();//recuperamos los datos desde SQLite
 
-        nivelBateriaActual = cargaBateria();//llamamos al metodo que consulta la carga actual de la bateria y lo guardamos en la variable
-
-        if (nivelBateriaActual < umbral){
-
-            Toast.makeText(this, "La bateria esta por debajo del umbral!!!", Toast.LENGTH_SHORT).show();
-
-            //***Envio de AVISOS***
-            //EnviarSMS();
-            //enviarMail("carloshernandezcrespo@gmail.com","Probando","Esto es una prueba.");
-        }
+        registerReceiver(bateryLevelBroadcastReceiver, bateryIntentFilter);
 
     }
-
 
 
     @Override
@@ -62,42 +50,51 @@ public class Inicio extends AppCompatActivity {
 
         //creamos la base de datos Tarea3
         //deleteDatabase("Tarea3");//para borrar la base de datos
-        db=openOrCreateDatabase("Tarea3", Context.MODE_PRIVATE, null);
+        db = openOrCreateDatabase("Tarea3", Context.MODE_PRIVATE, null);
         db.execSQL("CREATE TABLE IF NOT EXISTS configuracionesT3(ID TEXT PRIMARY KEY, umbral INT,lista_contactos TEXT, liata_emails TEXT);");
 
         //recuperaDatos();// se deshabilita ya que se cargará en el metodo onResume
 
+        //creamos el filtro del BroadcastReceiver y el propio BroadcastReceiver
+        bateryIntentFilter = new IntentFilter();
+        bateryIntentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        bateryLevelBroadcastReceiver = new BateryLevelBroadcastReceiver();
+
     }
 
-    //instanciamos un BroadcastReciber y sobreescribimos su metodo "onRecive"
-    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+
+    //**********************************************************************************************
+    //creamos una clase interna que implemente "BroadcastReceiver"
+    private class BateryLevelBroadcastReceiver extends BroadcastReceiver {
+
         @Override
-        public void onReceive(Context ctxt, Intent intent) {
-            nivelBateriaActual = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            batteryTxt.setText("Nivel de Batería actual: " + String.valueOf(nivelBateriaActual) + "%");
-        }
-    };
+        public void onReceive(Context context, Intent intent) {
 
+            int porcentaje = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            cambiaPorcentaje(porcentaje);
 
-    public int cargaBateria ()
-    {
-        try
-        {
-            IntentFilter batIntentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-            Intent battery = this.registerReceiver(null, batIntentFilter);
-            int nivelBateria = battery.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-            return nivelBateria;
-        }
-        catch (Exception e)
-        {
-            Toast.makeText(getApplicationContext(),
-                    "Error al obtener estado de la batería",
-                    Toast.LENGTH_SHORT).show();
-            return 0;
         }
     }
 
+    //metodo perteneciente a la clase principal que cambia el porcentaje y además manda las alertas
+    private void cambiaPorcentaje(int porcentaje) {
 
+        batteryTxt.setText("Nivel de Batería actual: " + String.valueOf(porcentaje) + "%");
+
+         if (porcentaje < umbral) {
+
+            Toast.makeText(this, "La bateria esta por debajo del umbral!!!", Toast.LENGTH_SHORT).show();
+
+            //***Envio de AVISOS***
+            //EnviarSMS();
+            //enviarMail("carloshernandezcrespo@gmail.com","Probando","Esto es una prueba.");
+        }
+
+        //cerramos para que no siga atendiendo los intent de cambio de nivel de bateria
+        unregisterReceiver(bateryLevelBroadcastReceiver);
+
+    }
+    //**********************************************************************************************
 
 
 
@@ -108,19 +105,19 @@ public class Inicio extends AppCompatActivity {
         startActivity(configura);
     }
 
-    public void enviarMail(String[] destinatarios, String asunto, String contenido){
+    public void enviarMail(String[] destinatarios, String asunto, String contenido) {
 
-        Intent chooser=null;
+        Intent chooser = null;
         Intent i = new Intent();
 
         i.setAction(Intent.ACTION_SEND);
         i.setData(Uri.parse("mailto:"));
-        String para[]=destinatarios;
-        i.putExtra(Intent.EXTRA_EMAIL,para);
-        i.putExtra(Intent.EXTRA_SUBJECT,asunto);
-        i.putExtra(Intent.EXTRA_TEXT,contenido);
+        String para[] = destinatarios;
+        i.putExtra(Intent.EXTRA_EMAIL, para);
+        i.putExtra(Intent.EXTRA_SUBJECT, asunto);
+        i.putExtra(Intent.EXTRA_TEXT, contenido);
         i.setType("message/rfc822");
-        chooser=i.createChooser(i,"Enviar Email");
+        chooser = i.createChooser(i, "Enviar Email");
         try {
             startActivity(i);
         } catch (ActivityNotFoundException e) {
@@ -130,26 +127,25 @@ public class Inicio extends AppCompatActivity {
     }
 
 
-    public void EnviarSMS(String[] destinatarios, String contenido){
+    public void EnviarSMS(String[] destinatarios, String contenido) {
 
         try {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage("636340858", null, contenido, null, null);
-            Toast.makeText(getApplicationContext(), "SMS enviado.",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "SMS enviado.", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),"SMS no enviado, por favor, inténtalo otra vez.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "SMS no enviado, por favor, inténtalo otra vez.", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
 
-    public void recuperaDatos(){
+    public void recuperaDatos() {
         Cursor c = db.rawQuery("SELECT * FROM configuracionesT3 WHERE ID = 'tarea3'", null);
 
-        if(c.getCount()==0) {
+        if (c.getCount() == 0) {
             Toast.makeText(this, "No hay datos guardados.", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            while(c.moveToNext()){
+        } else {
+            while (c.moveToNext()) {
                 if (c.getString(0).equals("tarea3")) {
 
                     umbral = Integer.parseInt(c.getString(1));//actualizamos el umbral
@@ -159,9 +155,9 @@ public class Inicio extends AppCompatActivity {
                     listaContactos = new String[PRElistaContactos.length];
                     int puntero = 0;
                     int posicionTelefono;
-                    for (String elem : PRElistaContactos){
-                        posicionTelefono  = elem.split(" \\(").length - 1;
-                        listaContactos[puntero] = elem.split(" \\(")[posicionTelefono].replace(")","");//actualizamos el array de numeros de contacto
+                    for (String elem : PRElistaContactos) {
+                        posicionTelefono = elem.split(" \\(").length - 1;
+                        listaContactos[puntero] = elem.split(" \\(")[posicionTelefono].replace(")", "");//actualizamos el array de numeros de contacto
                         puntero++;
                     }
                     listaEmails = c.getString(3).split(";");//actualizamos el array de emails
